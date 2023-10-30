ab2481240e34a4c899ee21e04fed9220567b0dd9
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-10-30 17:10:55 -0500

    Cambios para consulta de otros dominios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const userRouter = require("./routes/users");
 const cryptoRouter = require("./routes/crypto");
 
@@ -11,6 +12,7 @@ app.disable("x-powered-by");
 //Middleware
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 app.use("/", userRouter.router);
 app.use("/", cryptoRouter.router);
 
