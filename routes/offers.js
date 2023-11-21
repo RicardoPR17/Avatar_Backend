d3a747bf97bae7d35c98fb0519427f12adeb0b09
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-11-20 20:45:10 -0500

    Funcionalidad compra de ofertas completada

--- routes/offers.js
+++ routes/offers.js
@@ -2,10 +2,13 @@ const offers = require("../controllers/offers");
 const express = require("express");
 const router = express.Router();
 
-// Ruta GET: /api/offers
+// Rutas GET:
 router.get("/db/offers", offers.getAllOffers);
 router.get("/db/offers/seller/:email", offers.getSellerOffers);
 router.get("/db/offers/buyer/:email", offers.getBuyerOffers);
 router.get("/db/offers/:crypto", offers.getOffersByCrypto);
+
+// Rutas POST:
+router.post("/db/offers/buy", offers.buyOffer);
 
 module.exports = { router };
