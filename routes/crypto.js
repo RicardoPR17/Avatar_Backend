6c4f3fcb10dff69ceb4b1a3899f3ede5a75d8223
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-11-20 20:46:01 -0500

    Correcciones de estilo

--- routes/crypto.js
+++ routes/crypto.js
@@ -2,12 +2,9 @@ const cryptos = require("../controllers/crypto");
 const express = require("express");
 const router = express.Router();
 
-// Ruta GET: /db/cryptos
+// Rutas GET:
 router.get("/db/cryptos", cryptos.getCryptoData);
-
 router.get("/db/cryptos_last", cryptos.getLastCryptoData);
-
-// Ruta GET: /db/cryptos/:name
 router.get("/db/cryptos/:name", cryptos.getOneCrypto);
 
 
