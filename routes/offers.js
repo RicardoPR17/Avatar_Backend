f3b10e7309d57d46824fe7241f0f4b1bb88f1289
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-11-21 17:18:01 -0500

    Eliminación rutas de ofertas

--- routes/offers.js
+++ routes/offers.js
@@ -4,6 +4,5 @@ const router = express.Router();
 
 // Ruta GET: /api/offers
 router.get("/db/offers", offers.getAllOffers);
-router.get("/db/offers/:crypto", offers.getOffersByCrypto);
 
 module.exports = { router };
