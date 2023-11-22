0328b7b070d66f4978f8c9db3b325524bd19e676
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-11-22 14:45:22 -0500

    Eliminación de métodos en desuso

--- controllers/offers.js
+++ controllers/offers.js
@@ -19,61 +19,6 @@ const getAllOffers = async (req, res) => {
   res.status(200).json(query);
 };
 
-// The user must be able to search the offers filtered by the email of the seller
-const getSellerOffers = async (req, res) => {
-  const emailToSearch = req.params.email;
-  try {
-    if (!emailToSearch) throw new Error("Send an email to search the seller's offers");
-
-    const regexEmail = new RegExp(`^${emailToSearch}`, "i");
-
-    const offers = await offersDoc
-      .find({ seller: { $regex: regexEmail } })
-      .project({ _id: 0 })
-      .toArray();
-
-    if (offers.length === 0) throw new Error("Offers not found");
-    res.send(offers);
-  } catch (err) {
-    res.status(404).json({
-      error: err.message,
-    });
-  }
-};
-
-// The user must be able to search the offers filtered by his email as a buyer
-const getBuyerOffers = async (req, res) => {
-  const emailToSearch = req.params.email;
-  try {
-    if (!emailToSearch) throw new Error("Send an email to search the buyer's offers");
-
-    const regexEmail = new RegExp(`^${emailToSearch}`, "i");
-
-    const offers = await offersDoc
-      .find({ buyer: { $regex: regexEmail } })
-      .project({ _id: 0 })
-      .toArray();
-
-    if (offers.length === 0) throw new Error("Offers not found");
-    res.send(offers);
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
-};
-
-// The user must be able to search the offers filtered by the cryptocurrencies
-const getOffersByCrypto = async (req, res) => {
-  const cryptoName = req.params.crypto;
-  const cryptoSearch = new RegExp(`${cryptoName}`, "i"); // Regexp non case sensitive to search the crypto by his name
-  try {
-    const query = await offersDoc.find({ cryptocurrency: cryptoSearch, state: "Open" }).project({ _id: 0 }).toArray();
-    if (query.length === 0) throw new Error("Cryptocurrency not found in offers");
-    res.json(query);
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
-};
-
 // The user must be able to buy an offer and get the cryptos
 const buyOffer = async (req, res) => {
   const reqData = req.body;
@@ -88,6 +33,7 @@ const buyOffer = async (req, res) => {
 
     let offerState = offerData[0].state;
     if (offerState == "Closed") {
+      res.status(410);
       throw new Error("Sorry, someone buy this offer");
     }
 
